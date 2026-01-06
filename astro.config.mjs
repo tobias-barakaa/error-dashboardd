43be6f804c94314b4bc81ab6953b304aec18d469
tobias-barakaa <ngairato@gmail.com>
2026-01-06 13:51:32 +0300

    uninstall-tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  integrations: [tailwind()],
   output: 'static',
 });
